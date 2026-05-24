b01cf6c81d4114b927d2fd2813038940feb4678d
Adefolarin <adefolaringithub@gmail.com>
2026-05-25 00:49:25 +0100

    Done with js logic

---
+++ main.js
@@ -0,0 +1,19 @@
+const fulldate = document.getElementById("date");
+const clock = document.getElementById("clock");
+let toggle = false;
+const currentDate = new Date();
+function updateClock() {
+  clock.textContent = currentDate.toLocaleTimeString("default", {
+    hour12: !toggle,
+  });
+  const year = currentDate.getFullYear().toString();
+  const month = currentDate.toLocaleDateString("default", { month: "short" });
+  const date = currentDate.getDate().toString().padStart(2, 0);
+  const day = currentDate.toLocaleDateString("default", { weekday: "short" });
+  fulldate.textContent = day + " " + date + " " + month + " " + year;
+}
+setInterval(updateClock, 1000);
+function toggleClock() {
+  toggle = !toggle;
+  updateClock();
+}
